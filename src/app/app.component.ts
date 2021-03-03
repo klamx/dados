ecfc7abd5c55631fa58c0de323ab666e18429794
Alejandro <largoalejo@gmail.com>
2021-03-02 22:41:53 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dados';
+  value1: number;
+  value2: number;
+  src1: string;
+  src2: string;
+  equal: string = '';
+
+  ngOnInit() {
+    this.roll();
+  }
+
+  randomNumber(): number {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+
+  roll(): void {
+    this.value1 = this.randomNumber();
+    this.value2 = this.randomNumber();
+    this.src1 = `../assets/img/dice${this.value1}.png`;
+    this.src2 = `../assets/img/dice${this.value2}.png`;
+    this.value1 === this.value2
+      ? (this.equal = 'Ganaste')
+      : (this.equal = 'Perdiste');
+  }
 }
